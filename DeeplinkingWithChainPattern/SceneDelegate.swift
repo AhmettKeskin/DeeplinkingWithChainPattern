a05ba2d76b7ca69eba0ebdbf547c1c18b3791f7a
Ahmet Keskin <ahmet.keskin@trendyol.com>
2021-02-21 22:49:58 +0300

    add deeplink logic for killed state

--- DeeplinkingWithChainPattern/SceneDelegate.swift
+++ DeeplinkingWithChainPattern/SceneDelegate.swift
@@ -16,6 +16,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        // this logic is added for application killed state
+        DispatchQueue.global(qos: .userInteractive).async {
+            if let query = connectionOptions.urlContexts.first?.url.query {
+                deeplinkToOpen = self.createMap(from: query)
+            }
+        }
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
@@ -50,6 +57,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
         guard let query = URLContexts.first?.url.query else { return }
+        executeDeeplink(with: query)
+    }
+    
+    private func executeDeeplink(with query: String) {
         let params = createMap(from: query)
         DeeplinkManager.shared.navigate(with: params)
     }
--- DeeplinkingWithChainPattern/ViewControllers/HomepageViewController.swift
+++ DeeplinkingWithChainPattern/ViewControllers/HomepageViewController.swift
@@ -11,7 +11,15 @@ class HomepageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // If user launch app from deeplink | Killed state
+        if let deeplink = deeplinkToOpen {
+            DeeplinkManager.shared.navigate(with: deeplink)
+            deeplinkToOpen = nil
+        }
     }
 }
