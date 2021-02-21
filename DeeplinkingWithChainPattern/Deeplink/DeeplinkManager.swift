7f5fc4daedae9b97c9f6951fe37545b419deb36a
Ahmet Keskin <ahmet.keskin@trendyol.com>
2021-02-21 17:51:21 +0300

    arrange Deeplink manager with deeplink items and add navigate method

--- DeeplinkingWithChainPattern/Deeplink/DeeplinkManager.swift
+++ DeeplinkingWithChainPattern/Deeplink/DeeplinkManager.swift
@@ -5,6 +5,18 @@
 //  Created by Ahmet Keskin on 21.02.2021.
 //
 
-import Foundation
+import UIKit
 
-class DeeplinkManager {}
+class DeeplinkManager {
+    static let shared: DeeplinkManager = DeeplinkManager()
+    
+    private init() {}
+    
+    private let items: [DeeplinkableItem] = [ProductDetailDeeplinkItem(),
+                                             OrderDetailDeeplinkItem()]
+    
+    func navigate(with params: [String: Any]) {
+        guard let window = UIApplication.shared.windows.first else { return }
+        items.first(where: { $0.isSatisfied(by: params)})?.execute(window, params: params)
+    }
+}
