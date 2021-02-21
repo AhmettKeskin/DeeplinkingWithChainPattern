b920847dce67f61787615bd3cbc20e1f2214fd86
Ahmet Keskin <ahmet.keskin@trendyol.com>
2021-02-21 16:42:32 +0300

    Initial Commit

---
+++ DeeplinkingWithChainPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeeplinkingWithChainPattern
+//
+//  Created by Ahmet Keskin on 21.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
