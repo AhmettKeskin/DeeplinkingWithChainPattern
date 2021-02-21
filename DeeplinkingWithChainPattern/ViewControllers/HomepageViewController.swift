5e08f55ec2f9e269bb9200a0a02a3ee21de37ba3
Ahmet Keskin <ahmet.keskin@trendyol.com>
2021-02-21 17:13:03 +0300

    create dummy viewcontrollers

---
+++ DeeplinkingWithChainPattern/ViewControllers/HomepageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomepageViewController.swift
+//  DeeplinkingWithChainPattern
+//
+//  Created by Ahmet Keskin on 21.02.2021.
+//
+
+import UIKit
+
+class HomepageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
